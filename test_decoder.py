75a4f2a897a6c9f15bb596f0cdc3c8881e7e11e0
maximoskp <maximoskalpap@gmail.com>
2023-10-24 11:21:02 +0300

    before transformer test

--- test_decoder.py
+++ test_decoder.py
@@ -1,19 +1,20 @@
 from numpy import random
-from transformer.layers import Encoder
+from transformer.layers import Decoder
  
-enc_vocab_size = 20 # Vocabulary size for the encoder
+dec_vocab_size = 20  # Vocabulary size for the decoder
 input_seq_length = 5  # Maximum length of the input sequence
 h = 8  # Number of self-attention heads
 d_k = 64  # Dimensionality of the linearly projected queries and keys
 d_v = 64  # Dimensionality of the linearly projected values
 d_ff = 2048  # Dimensionality of the inner fully connected layer
 d_model = 512  # Dimensionality of the model sub-layers' outputs
-n = 6  # Number of layers in the encoder stack
+n = 6  # Number of layers in the decoder stack
  
 batch_size = 64  # Batch size from the training process
 dropout_rate = 0.1  # Frequency of dropping the input units in the dropout layers
  
 input_seq = random.random((batch_size, input_seq_length))
+enc_output = random.random((batch_size, input_seq_length, d_model))
  
-encoder = Encoder(enc_vocab_size, input_seq_length, h, d_k, d_v, d_model, d_ff, n, dropout_rate)
-print(encoder(input_seq, None, True))
+decoder = Decoder(dec_vocab_size, input_seq_length, h, d_k, d_v, d_model, d_ff, n, dropout_rate)
+print(decoder(input_seq, enc_output, None, True))
---
+++ transformer/models.py
@@ -0,0 +1,39 @@
+from tensorflow import math, cast, float32, linalg, ones, maximum, newaxis
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense
+from transformer.layers import Encoder, Decoder
+
+class TransformerModel(Model):
+    def __init__(self, enc_vocab_size, dec_vocab_size, enc_seq_length, dec_seq_length, h, d_k, d_v, d_model, d_ff_inner, n, rate, **kwargs):
+        super(TransformerModel, self).__init__(**kwargs)
+        self.encoder = Encoder(enc_vocab_size, enc_seq_length, h, d_k, d_v, d_model, d_ff_inner, n, rate)
+        self.decoder = Decoder(dec_vocab_size, dec_seq_length, h, d_k, d_v, d_model, d_ff_inner, n, rate)
+        self.model_last_layer = Dense(dec_vocab_size)
+    # end init
+
+    def padding_mask(self, input):
+        mask = math.equal(input, 0)
+        mask = cast(mask, float32)
+        # shape needs to match attention maps
+        return mask[:, newaxis, newaxis, :]
+    # end padding_mask
+
+    def lookahead_mask(self, shape):
+        mask = 1 - linalg.band_part(ones((shape, shape)), -1, 0)
+        return mask
+    # end lookahead_mask
+
+    def call(self, encoder_input, decoder_input, training):
+        enc_padding_mask = self.padding_mask(encoder_input)
+        
+        dec_padding_mask = self.padding_mask(decoder_input)
+        dec_in_lookahead_mask = self.lookahead_mask(decoder_input.shape[1])
+        dec_in_lookahead_mask = maximum(dec_padding_mask, dec_in_lookahead_mask)
+
+        encoder_output = self.encoder(encoder_input)
+        decoder_output = self.decoder(decoder_input, encoder_output, dec_in_lookahead_mask, enc_padding_mask, training)
+
+        model_output = self.model_last_layer(decoder_output)
+
+        return model_output
+    # end call
