28342678392fff58767b8495351d031de96546c7
maximoskp <maximoskalpap@gmail.com>
2023-10-29 08:01:08 +0200

    first attempt for model-separate transformer

--- transformer/layers.py
+++ transformer/layers.py
@@ -118,11 +118,11 @@ class Decoder(Layer):
         self.decoder_layer = [DecoderLayer(sequence_length, h, d_k, d_v, d_model, d_ff, rate) for _ in range(n)]
     # end init
 
-    def call(self, output_target, encoder_input, lookahead_mask, padding_mask, training):
+    def call(self, output_target, encoder_output, lookahead_mask, padding_mask, training):
         pos_encoding_output = self.pos_encoding(output_target) # (num_sentences, seq_length, d_model)
         x = self.dropout(pos_encoding_output, training=training)
         for i, layer in enumerate(self.decoder_layer):
-            x = layer(x, encoder_input, lookahead_mask, padding_mask, training)
+            x = layer(x, encoder_output, lookahead_mask, padding_mask, training)
         return x
     # end call
 # end class Decoder
--- transformer/models.py
+++ transformer/models.py
@@ -35,3 +35,76 @@ class TransformerModel(Model):
         model_output = self.model_last_layer(decoder_output)
         return model_output
     # end call
+# end class TransformerModel
+
+class EncoderModel(Model):
+    def __init__(self, enc_vocab_size, enc_seq_length, h, d_k, d_v, d_model, d_ff_inner, n, rate, **kwargs):
+        super(EncoderModel, self).__init__(**kwargs)
+        self.encoder = Encoder(enc_vocab_size, enc_seq_length, h, d_k, d_v, d_model, d_ff_inner, n, rate)
+    # end init
+
+    def padding_mask(self, input):
+        mask = math.equal(input, 0)
+        mask = cast(mask, float32)
+        # shape needs to match attention maps
+        return mask[:, newaxis, newaxis, :]
+    # end padding_mask
+
+    def call(self, encoder_input, training):
+        enc_padding_mask = self.padding_mask(encoder_input)
+        return self.encoder(encoder_input, enc_padding_mask, training)
+    # end call
+# end class EncoderModel
+
+class DecoderModel(Model):
+    def __init__(self, dec_vocab_size, dec_seq_length, h, d_k, d_v, d_model, d_ff_inner, n, rate, **kwargs):
+        super(DecoderModel, self).__init__(**kwargs)
+        self.decoder = Decoder(dec_vocab_size, dec_seq_length, h, d_k, d_v, d_model, d_ff_inner, n, rate)
+        self.model_last_layer = Dense(dec_vocab_size)
+    # end init
+
+    def padding_mask(self, input):
+        mask = math.equal(input, 0)
+        mask = cast(mask, float32)
+        # shape needs to match attention maps
+        return mask[:, newaxis, newaxis, :]
+    # end padding_mask
+
+    def lookahead_mask(self, shape):
+        mask = 1 - linalg.band_part(ones((shape, shape)), -1, 0)
+        return mask
+    # end lookahead_mask
+
+    def call(self, decoder_input, encoder_output, enc_padding_mask, training):
+        # if no encoder input is provided, encoder_output and enc_padding_mask should be set to trivial values
+        dec_padding_mask = self.padding_mask(decoder_input)
+        dec_in_lookahead_mask = self.lookahead_mask(decoder_input.shape[1])
+        dec_in_lookahead_mask = maximum(dec_padding_mask, dec_in_lookahead_mask)
+
+        decoder_output = self.decoder(decoder_input, encoder_output, dec_in_lookahead_mask, enc_padding_mask, training)
+        model_output = self.model_last_layer(decoder_output)
+        return model_output
+    # end call
+# end class DecoderModel
+
+class TransformerEncDecModel(Model):
+    def __init__(self, encoder_model, decoder_model, **kwargs):
+        super(TransformerEncDecModel, self).__init__(**kwargs)
+        self.encoder_model = encoder_model
+        self.decoder_model = decoder_model
+    # end init
+
+    def padding_mask(self, input):
+        mask = math.equal(input, 0)
+        mask = cast(mask, float32)
+        # shape needs to match attention maps
+        return mask[:, newaxis, newaxis, :]
+    # end padding_mask
+
+    def call(self, encoder_input, decoder_input, training):
+        enc_padding_mask = self.padding_mask(encoder_input)
+        encoder_output = self.encoder_model(encoder_input, training)
+        model_output = self.decoder_model(decoder_input, encoder_output, enc_padding_mask, training)
+        return model_output
+    # end call
+# end class TransformerEncDecModel
